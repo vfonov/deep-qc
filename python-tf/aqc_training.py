ab6ba2688ea0f4c8996db39c339de3a03f8f29b4
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2019-07-30 16:38:25 -0400

    separated model into a separate file, the model is changed too

--- python-tf/aqc_training.py
+++ python-tf/aqc_training.py
@@ -4,182 +4,164 @@
 # @date 28/07/2019
 
 from __future__ import absolute_import, division, print_function, unicode_literals
+import argparse
+from datetime import datetime # for tensorboard
+import os
 
-#import os
 import tensorflow as tf
 from tensorflow.keras import layers
 import numpy as np
 
+# local
+from model import create_qc_model
+
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-tf.compat.v1.enable_eager_execution()
-
-BATCH_SIZE = 32
-#steps_per_epoch = 200
-total_number_of_epochs = 100
-training_frac=90
-validation_frac=2
-testing_frac=8
-
-filenames = [ 'deep_qc_data_0.tfrecord', 'deep_qc_data_1.tfrecord', 'deep_qc_data_2.tfrecord', 'deep_qc_data_3.tfrecord'  ]
-raw_ds = tf.data.TFRecordDataset( filenames )
-
-# hardcoded
-n_subj=3331
-n_samples=57848
-
-# random permutation
-np.random.seed(42) # specify random seed, so that split is consistent
-
-# initialize subject-based split
-all_subjects=np.random.permutation(n_subj)
-
-train_subjects = tf.convert_to_tensor( all_subjects[0:n_subj*training_frac//100] )
-validation_subjects = tf.convert_to_tensor(  all_subjects[n_subj*training_frac//100:n_subj*training_frac//100+n_subj*training_frac//100] )
-testing_subjects = tf.convert_to_tensor( all_subjects[n_subj*training_frac//100+n_subj*training_frac//100:-1] )
-
-feature_description = {
-    'img1_jpeg': tf.io.FixedLenFeature([], tf.string, default_value=''),
-    'img2_jpeg': tf.io.FixedLenFeature([], tf.string, default_value=''),
-    'img3_jpeg': tf.io.FixedLenFeature([], tf.string, default_value=''),
-    'qc':   tf.io.FixedLenFeature([], tf.int64,  default_value=0 ),
-    'subj': tf.io.FixedLenFeature([], tf.int64,  default_value=0 )
-    }
-
-def _parse_feature(i):
-    # Parse the input tf.Example proto using the dictionary above.
-    return tf.io.parse_single_example(i, feature_description)
-
-def _decode_jpeg(a):
-    img1 = tf.cast(tf.image.decode_jpeg(a['img1_jpeg'], channels=1),dtype=tf.float32)/127.5-1.0
-    img2 = tf.cast(tf.image.decode_jpeg(a['img2_jpeg'], channels=1),dtype=tf.float32)/127.5-1.0
-    img3 = tf.cast(tf.image.decode_jpeg(a['img3_jpeg'], channels=1),dtype=tf.float32)/127.5-1.0
-
-    return  {'View1':img1, 'View2':img2, 'View3':img3},{'qc':a['qc'], 'subj':a['subj']}
+#tf.compat.v1.enable_eager_execution()
 
 
-parsed_ds = raw_ds.map(_parse_feature, num_parallel_calls=AUTOTUNE )
+def parse_options():
+    parser = argparse.ArgumentParser(description='Convert QC data into tfrecords',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-# we want to split the database based on subject id's not sample id, since the same subject can be present multiple times
-# with slightly different result
-training_ds = parsed_ds.filter(lambda x: tf.reduce_any(tf.math.equal(tf.expand_dims(x['subj'],0),tf.expand_dims(train_subjects,1)) )) # hack
-training_ds = training_ds.map(_decode_jpeg, num_parallel_calls=AUTOTUNE )
-training_ds = training_ds.shuffle(buffer_size=2000) # TODO: determine optimal buffer size
-training_ds = training_ds.repeat()
-training_ds = training_ds.batch(BATCH_SIZE)
-training_ds = training_ds.prefetch(buffer_size=AUTOTUNE)
+    parser.add_argument("--batch_size", type=int, default=32,
+                        help="Batch size")
+    parser.add_argument("--data", default="deep_qc_shuffled.tfrecord",
+                        help="Batch size")
+    parser.add_argument("--load", type=str,
+                        help="load weights from file")
+    parser.add_argument("--epochs", type=int, default=100,
+                        help="Batch size")
+    parser.add_argument("--output", type=str, default='qc_mobile_net_v2'
+                        help="Output prefix")
 
-testing_ds = parsed_ds.filter(lambda x: tf.reduce_any(tf.math.equal(tf.expand_dims(x['subj'],0),tf.expand_dims(testing_subjects,1)) ))
-testing_ds = testing_ds.map(_decode_jpeg,  num_parallel_calls=AUTOTUNE )
-testing_ds = testing_ds.batch(BATCH_SIZE)
+    params = parser.parse_args()
+    return params
 
-validation_ds = parsed_ds.filter(lambda x: tf.reduce_any(tf.math.equal(tf.expand_dims(x['subj'],0),tf.expand_dims(validation_subjects,1)) ))
-validation_ds = validation_ds.map(_decode_jpeg,  num_parallel_calls=AUTOTUNE )
-validation_ds = validation_ds.batch(BATCH_SIZE)
-validation_ds = validation_ds.prefetch(buffer_size=AUTOTUNE)
+if __name__ == '__main__':
+    params = parse_options()
 
-# create Keras model
-inner_model = tf.keras.applications.MobileNetV2(input_shape=(224, 224, 1), include_top=False, weights=None)
-#inner_model = tf.keras.applications.ResNet50(input_shape=(224, 224, 1), include_top=False, weights=None)
+    BATCH_SIZE = params.batch_size
+    #steps_per_epoch = 200
+    total_number_of_epochs = params.epochs
+    training_frac=90
+    validation_frac=2
+    testing_frac=8
 
-print(inner_model.summary())
+    #filenames = [ 'deep_qc_data_0.tfrecord', 'deep_qc_data_1.tfrecord', 'deep_qc_data_2.tfrecord', 'deep_qc_data_3.tfrecord'  ]
+    filenames = [ params.data ]
+    raw_ds = tf.data.TFRecordDataset( filenames )
 
-# create registration classification model
-im1 = layers.Input(shape=(224, 224, 1),name='View1')
-im2 = layers.Input(shape=(224, 224, 1),name='View2')
-im3 = layers.Input(shape=(224, 224, 1),name='View3')
+    # hardcoded
+    n_subj=3331
+    n_samples=57848
 
-# use the same inner model for three images
-x1 = inner_model(im1) # each will be  6, 6, 1280 ?
-x2 = inner_model(im2)
-x3 = inner_model(im3)
+    # random permutation
+    np.random.seed(42) # specify random seed, so that split is consistent
 
-# join together
-x = layers.Concatenate(axis=-1)([x1,x2,x3])
+    # initialize subject-based split
+    all_subjects=np.random.permutation(n_subj)
 
-# learn spatial features of the merged layers
-x = layers.Conv2D(128,(1,1),activation='relu')(x)
-x = layers.BatchNormalization()(x)
-x = layers.Conv2D(16,(1,1),activation='relu')(x)
-x = layers.BatchNormalization()(x)
-x = layers.Conv2D(4,(1,1),activation='relu')(x)
-x = layers.BatchNormalization()(x)
-# output per region activation
-x = layers.Conv2D(1,(1,1),activation='sigmoid')(x)
-x = layers.AveragePooling2D(pool_size=(7, 7))(x) # average across all image
+    train_subjects = tf.convert_to_tensor( all_subjects[0:n_subj*training_frac//100] )
+    validation_subjects = tf.convert_to_tensor(  all_subjects[n_subj*training_frac//100:n_subj*training_frac//100+n_subj*training_frac//100] )
+    testing_subjects = tf.convert_to_tensor( all_subjects[n_subj*training_frac//100+n_subj*training_frac//100:-1] )
 
-# end of spatial preprocessing
-x = layers.Flatten(name='qc')(x)
-#x = layers.Dense(128,activation='relu')(x)
-#x = layers.Dense(1,activation='sigmoid',name='qc')(x)
-#x = layers.Dense(2)(x)
-#x = layers.Activation('softmax',name='qc')(x)
+    feature_description = {
+        'img1_jpeg': tf.io.FixedLenFeature([], tf.string, default_value=''),
+        'img2_jpeg': tf.io.FixedLenFeature([], tf.string, default_value=''),
+        'img3_jpeg': tf.io.FixedLenFeature([], tf.string, default_value=''),
+        'qc':   tf.io.FixedLenFeature([], tf.int64,  default_value=0 ),
+        'subj': tf.io.FixedLenFeature([], tf.int64,  default_value=0 )
+        }
 
-model = tf.keras.models.Model(inputs=[im1,im2,im3], outputs=x)
+    def _parse_feature(i):
+        # Parse the input tf.Example proto using the dictionary above.
+        return tf.io.parse_single_example(i, feature_description)
 
-# true negative rate metric
-model.compile(optimizer=tf.keras.optimizers.Nadam(),
-              loss='binary_crossentropy', # tf.keras.losses.sparse_categorical_crossentropy,
-              metrics=["accuracy", tf.keras.metrics.AUC()])
+    def _decode_jpeg(a):
+        img1 = tf.cast(tf.image.decode_jpeg(a['img1_jpeg'], channels=1),dtype=tf.float32)/127.5-1.0
+        img2 = tf.cast(tf.image.decode_jpeg(a['img2_jpeg'], channels=1),dtype=tf.float32)/127.5-1.0
+        img3 = tf.cast(tf.image.decode_jpeg(a['img3_jpeg'], channels=1),dtype=tf.float32)/127.5-1.0
 
-print(model.summary())
-
-#tf.keras.utils.plot_model(model, to_file='whole_model.png')
+        return  {'View1':img1, 'View2':img2, 'View3':img3},{'qc':a['qc'], 'subj':a['subj']}
 
 
-# setup training process
-# start training
-from datetime import datetime
-checkpoint_path = "training/cp.ckpt"
+    parsed_ds = raw_ds.map(_parse_feature, num_parallel_calls=AUTOTUNE )
 
-# Create checkpoint callback
-cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
-                                                 save_weights_only=True,
-                                                 save_best_only=True,
-                                                 mode='max',
-                                                 monitor='val_auc',
-                                                 verbose=0)
-# logging to tensorboard
-logdir="runs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    # we want to split the database based on subject id's not sample id, since the same subject can be present multiple times
+    # with slightly different result
+    training_ds = parsed_ds.filter(lambda x: tf.reduce_any(tf.math.equal(tf.expand_dims(x['subj'],0),tf.expand_dims(train_subjects,1)) )) # hack
+    training_ds = training_ds.map(_decode_jpeg, num_parallel_calls=AUTOTUNE )
+    training_ds = training_ds.shuffle(buffer_size=2000) # TODO: determine optimal buffer size, input should be already pre-shuffled
+    training_ds = training_ds.repeat()
+    training_ds = training_ds.batch(BATCH_SIZE)
+    training_ds = training_ds.prefetch(buffer_size=AUTOTUNE)
 
-tensorboard_callback = tf.keras.callbacks.TensorBoard(
-    log_dir=logdir,
-    histogram_freq=1,
-    write_grads=True,
-    write_images=True,
-    update_freq='batch',
-    profile_batch=0,
-    batch_size=BATCH_SIZE)
+    testing_ds = parsed_ds.filter(lambda x: tf.reduce_any(tf.math.equal(tf.expand_dims(x['subj'],0),tf.expand_dims(testing_subjects,1)) ))
+    testing_ds = testing_ds.map(_decode_jpeg,  num_parallel_calls=AUTOTUNE )
+    testing_ds = testing_ds.batch(BATCH_SIZE)
 
-# create scheduler
+    validation_ds = parsed_ds.filter(lambda x: tf.reduce_any(tf.math.equal(tf.expand_dims(x['subj'],0),tf.expand_dims(validation_subjects,1)) ))
+    validation_ds = validation_ds.map(_decode_jpeg,  num_parallel_calls=AUTOTUNE )
+    validation_ds = validation_ds.batch(BATCH_SIZE)
+    validation_ds = validation_ds.prefetch(buffer_size=AUTOTUNE)
 
-def step_decay_schedule(initial_lr=1e-4, decay_factor=0.9, step_size=10):
-    '''
-    Wrapper function to create a LearningRateScheduler with step decay schedule.
-    '''
-    def schedule(epoch):
-        return initial_lr * (decay_factor ** np.floor(epoch/step_size))
-    return tf.keras.callbacks.LearningRateScheduler(schedule)
+    model = create_qc_model(input_shape=(224, 224, 1),dropout=True,filters=32)
 
+    model.compile(optimizer=tf.keras.optimizers.Nadam(),
+                loss='binary_crossentropy', # tf.keras.losses.sparse_categorical_crossentropy,
+                metrics=[ "accuracy", tf.keras.metrics.AUC()] )
 
-lr_sched = step_decay_schedule(initial_lr=1e-5, decay_factor=0.9, step_size=10)
+    print(model.summary())
 
-steps_per_epoch=n_samples//BATCH_SIZE
+    #tf.keras.utils.plot_model(model, to_file='whole_model.png')
+    # setup training process
+    # start training
+    checkpoint_path = "training/cp.ckpt"
 
-# load model trained previosly
-model.load_weights('qc_mobilenet')
-# continue training
+    # Create checkpoint callback
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                    save_weights_only=True,
+                                                    save_best_only=True,
+                                                    mode='max',
+                                                    monitor='val_auc',
+                                                    verbose=0)
+    # logging to tensorboard
+    logdir="runs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 
-train_hist=model.fit(training_ds,
-  validation_data=validation_ds,
-  epochs=total_number_of_epochs,
-  steps_per_epoch=steps_per_epoch,
-  callbacks=[tensorboard_callback, cp_callback, lr_sched ]  # lr_sched
-  )
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=logdir,
+        histogram_freq=1,
+        write_grads=True,
+        write_images=True,
+        update_freq='batch',
+        profile_batch=0,
+        batch_size=BATCH_SIZE)
 
+    # create scheduler
+    def step_decay_schedule(initial_lr=1e-4, decay_factor=0.9, step_size=10):
+        '''
+        Wrapper function to create a LearningRateScheduler with step decay schedule.
+        '''
+        def schedule(epoch):
+            return initial_lr * (decay_factor ** np.floor(epoch/step_size))
+        return tf.keras.callbacks.LearningRateScheduler(schedule)
 
-# save final weights
-model.save_weights('qc_mobilenet_s2')
+    lr_sched = step_decay_schedule(initial_lr=1e-5, decay_factor=0.9, step_size=10)
 
-# save whole model
-model.save('qc_mobilenet_s2_model.h5')
+    steps_per_epoch=n_samples//BATCH_SIZE
+    # load model trained previosly
+    if params.load is not None:
+        model.load_weights(params.load)
+
+    train_hist=model.fit(training_ds,
+        validation_data=validation_ds,
+        epochs=total_number_of_epochs,
+        steps_per_epoch=steps_per_epoch,
+        callbacks=[tensorboard_callback, cp_callback, lr_sched ]  # lr_sched
+        )
+
+    # save final weights
+    model.save_weights(params.output)
+    # save whole model
+    model.save(params.output+'_model.h5')
---
+++ python-tf/model.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# @author Vladimir S. FONOV
+# @date 28/07/2019
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+#import os
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+
+
+def augment_inner_model(inner_model,x,out_filters=32,dropout=True):
+    # pass through inner model to extract features
+    x = inner_model(x)
+
+    # ResNetX style learning of high order features
+    # TODO: replace with SeparableConv2D ?
+    x = layers.Conv2D(out_filters, (1,1), activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.DepthwiseConv2D( (3,3), activation='relu', padding='valid')(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv2D(out_filters, (1,1), activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+
+    if dropout:
+        x = layers.SpatialDropout2D()(x)
+    
+    return x
+
+def create_qc_model(input_shape=(224, 224, 1), dropout=True, filters=32):
+
+    # use existing create Keras model as base
+    inner_model = tf.keras.applications.MobileNetV2(input_shape=input_shape, include_top=False, weights=None)
+    #inner_model = tf.keras.applications.ResNet50(input_shape=(224, 224, 1), include_top=False, weights=None)
+
+    # create registration classification model
+    im1 = layers.Input(shape=(224, 224, 1), name='View1')
+    im2 = layers.Input(shape=(224, 224, 1), name='View2')
+    im3 = layers.Input(shape=(224, 224, 1), name='View3')
+
+    # use the same inner model for three images
+    x1 = augment_inner_model(inner_model, im1, dropout=dropout, out_filters=filters) 
+    x2 = augment_inner_model(inner_model, im2, dropout=dropout, out_filters=filters)
+    x3 = augment_inner_model(inner_model, im3, dropout=dropout, out_filters=filters)
+
+    # join together
+    x = layers.Concatenate(axis=-1)( [x1,x2,x3] )
+
+    # learn spatial features of the merged layers
+    x = layers.Conv2D(filters*3, (1,1), activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv2D(16, (1,1), activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(4, (1,1), activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+    # output per region activation
+
+    x = layers.Conv2D(1,(1,1),activation='sigmoid')(x)
+    x = layers.GlobalAveragePooling2D()(x) # average across all image
+
+    # end of spatial preprocessing
+    x = layers.Flatten(name='qc')(x)
+
+    out_model = tf.keras.models.Model(inputs=[im1,im2,im3], outputs=x)
+
+    return out_model
+    
