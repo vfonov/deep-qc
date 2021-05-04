2b5dd4947f726e33edf4a5c1760b5b9d80556446
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2021-05-04 13:07:26 -0400

    WIP: refactored cross-validation to collect more metrics

--- python/aqc_training.py
+++ python/aqc_training.py
@@ -21,6 +21,84 @@ from torch import optim
 from torch.utils.tensorboard import SummaryWriter
 
 
+def run_validation_testing_loop(dataloader, model, loss_fn=nn.functional.cross_entropy,details=False ):
+    """
+    Run Validation/Testing loop
+    return validation_dict, validation_log
+    """
+    res = {}
+
+    ids     = []
+    _preds  = np.zeros(0,dtype='int')
+    _labels = np.zeros(0,dtype='int')
+    _scores = np.zeros(0)
+
+    val_loss  = 0.0
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import precision_recall_fscore_support
+    from sklearn.metrics import roc_auc_score
+
+    with torch.no_grad():
+        for v_batch, v_sample_batched in enumerate(dataloader):
+            inputs = v_sample_batched['image' ].cuda()
+            labels = v_sample_batched['status'].cuda()
+
+            outputs=model(inputs)
+            loss = loss_fn(outputs, labels)
+            outputs = nn.functional.softmax(outputs,1)
+            _, preds = torch.max(outputs, 1)
+
+            val_loss += float(loss) * inputs.size(0)
+
+            ids.extend(v_sample_batched['id'])
+
+            _preds=np.concatenate((_preds,  preds.cpu().numpy()))
+            _labels=np.concatenate((_labels,labels.cpu().numpy()))
+            _scores=np.concatenate((_scores,outputs[:,1].cpu().numpy()))
+
+        # (?)
+        val_loss /= len(ids)
+
+        _ap   = float(np.sum( (_labels == 1)))
+        _an   = float(np.sum( (_labels == 0)))
+
+        # calculating true positive and true negative
+        _tpr = float(np.sum( (_preds == 1)*(_labels==1)))
+        _tnr = float(np.sum( (_preds == 0)*(_labels==0)))
+
+        if _ap>0:
+            _tpr  /= _ap
+        else:
+            _tpr = 0.0
+
+        if _an>0:
+            _tnr  /= _an
+        else:
+            _tnr = 0.0
+
+        prec,recall,fbeta,_ = precision_recall_fscore_support(_labels,_preds,average='binary')
+
+        res['summary'] = { 'loss': val_loss,
+                            'acc': accuracy_score(_labels,_preds),
+                            'prec': prec,
+                            'F': fbeta,
+                            'recall': recall,
+                            'auc': roc_auc_score(_labels,_scores),
+                            'tpr': _tpr,
+                            'tnr': _tnr
+                        }
+        
+        if details:
+            res['details'] = {
+                'ids':ids,
+                'preds':_preds.tolist(),
+                'labels':_labels.tolist(),
+                'scores':_scores.tolist()
+            }
+    return res
+
+
+
 def parse_options():
 
     parser = argparse.ArgumentParser(description='Train automated QC',
@@ -75,19 +153,13 @@ if __name__ == '__main__':
     training, validation, testing = split_dataset(all_samples, fold=params.fold, folds=params.folds, validation=val_subjects, 
         shuffle=True, seed=params.seed)
 
-    print("Training\t {} ".format(len(training)))
-    print("Validation\t{} ".format(len(validation)))
-    print("Testing\t{} ".format(len(testing)))
-
     train_dataset    = QCDataset(training, data_prefix, use_ref=use_ref)
     validate_dataset = QCDataset(validation, data_prefix, use_ref=use_ref)
     testing_dataset  = QCDataset(testing, data_prefix, use_ref=use_ref)
     
-    print("Training\t{} samples {} unique subjects".format(len(train_dataset),train_dataset.n_subjects()))
-    print("Validation\t{} samples {} unique subjects".format(len(validate_dataset),validate_dataset.n_subjects()))
-    print("Testing\t{} samples {} unique subjects".format(len(testing_dataset),testing_dataset.n_subjects()))
-
-    dataset_size = len(train_dataset)
+    print("Training    {} samples, {} unique subjects".format(len(train_dataset),train_dataset.n_subjects()))
+    print("Validation  {} samples, {} unique subjects".format(len(validate_dataset),validate_dataset.n_subjects()))
+    print("Testing     {} samples, {} unique subjects".format(len(testing_dataset),testing_dataset.n_subjects()))
 
     training_dataloader = DataLoader(train_dataset, 
                           batch_size=params.batch_size,
@@ -104,7 +176,6 @@ if __name__ == '__main__':
 
     model = get_qc_model(params, use_ref=use_ref, pretrained=params.pretrained)    
 
-
     model     = model.cuda()
     #criterion = nn.CrossEntropyLoss()
     if params.adam:
@@ -118,38 +189,26 @@ if __name__ == '__main__':
     writer = SummaryWriter()
 
     global_ctr = 0
+
     best_model_acc = copy.deepcopy(model.state_dict())
     best_model_tpr = copy.deepcopy(model.state_dict())
     best_model_tnr = copy.deepcopy(model.state_dict())
+    best_model_auc = copy.deepcopy(model.state_dict())
 
     best_acc = 0.0
     best_tnr = 0.0
     best_tpr = 0.0
-    validation_period = 200
+    best_auc = 0.0
 
     training_log = []
     validation_log = []
     testing_log = []
-    testing_details = []
 
     for epoch in range(params.n_epochs):
-        print('Epoch {}/{}'.format(epoch, params.n_epochs - 1))
+        print('Epoch {}/{}'.format(epoch+1, params.n_epochs))
         print('-' * 10)
 
         model.train(True)  # Set model to training mode
-
-        # for stats
-        running_loss = 0.0
-        running_acc  = 0.0
-
-        val_running_loss = 0.0
-        val_running_acc  = 0.0
-
-        val_running_tpr  = 0.0
-        val_running_fpr  = 0.0
-        val_running_tnr  = 0.0
-
-        model.train(True)
         for i_batch, sample_batched in enumerate(training_dataloader):
             inputs = sample_batched['image'].cuda()
             labels = sample_batched['status'].cuda()
@@ -176,111 +235,74 @@ if __name__ == '__main__':
             writer.add_scalars('{}/training'.format(params.output),
                                 log, global_ctr)
             log['ctr']=global_ctr
+            log['epoch']=epoch
+
             training_log.append(log)
             global_ctr += 1
 
         model.train(False)  # Set model to evaluation mode
         # run validation at the end of epoch
-        with torch.no_grad():
-            val_loss  = 0.0
-            val_acc   = 0.0
+        if len(validation)>0:
+            val_info = run_validation_testing_loop(validation_dataloader,model,details=False)
+            val=val_info['summary']
 
-            val_tp    = 0.0
-            val_tn    = 0.0
-            val_ap    = 0.0
-            val_an    = 0.0
-            val_fp    = 0.0
-
-            for v_batch, v_sample_batched in enumerate(validation_dataloader):
-                inputs = v_sample_batched['image' ].cuda()
-                labels = v_sample_batched['status'].cuda()
-
-                outputs=model(inputs)
-                loss = nn.functional.cross_entropy(outputs, labels)
-                outputs = nn.functional.softmax(outputs,1)
-                _, preds = torch.max(outputs, 1)
-
-                val_loss += float(loss) * inputs.size(0)
-                val_acc  += float(torch.sum(preds == labels))
-
-                # calculating true positive and true negative
-                val_tp   += float(torch.sum( (preds == 1)*(labels==1)))
-                val_fp   += float(torch.sum( (preds == 1)*(labels==0)))
-                val_tn   += float(torch.sum( (preds == 0)*(labels==0)))
-
-                val_ap   += float(torch.sum( (labels == 1)))
-                val_an   += float(torch.sum( (labels == 0)))
-
-            # (?)
-            val_loss /= len(validate_dataset)
-            val_acc  /= len(validate_dataset)
-
-            if val_ap>0:
-                val_tp  /= val_ap
-            else:
-                val_tp = 0.0
-
-            if val_an>0:
-                val_tn  /= val_an
-                val_fp  /= val_an
-            else:
-                val_tn = 0.0
+            if not params.adam:
+                scheduler.step()
             
-            print("{} - {},{}".format(global_ctr, val_loss, val_acc))
+            if val['acc'] > best_acc:
+                    best_acc = val['acc']
+                    best_model_acc = copy.deepcopy(model.state_dict())
 
-            log = {'loss': val_loss,
-                                'acc':  val_acc,
-                                'tpr':  val_tp,
-                                'tnr':  val_tn,
-                                'fpr':  val_fp
-                                }
+            if val['tpr'] > best_tpr:
+                    best_tpr = val['tpr']
+                    best_model_tpr = copy.deepcopy(model.state_dict())
 
+            if val['tnr'] > best_tnr:
+                    best_tnr = val['tnr']
+                    best_model_tnr = copy.deepcopy(model.state_dict())
+
+            if val['auc'] > best_auc:
+                    best_auc = val['auc']
+                    best_model_auc = copy.deepcopy(model.state_dict())
+            
             writer.add_scalars('{}/validation_epoch'.format(params.output), 
-                                log,
+                                val,
                                 epoch)
 
-            log['ctr']   = global_ctr
-            log['epoch'] = epoch
-            validation_log.append(log)
+            val['epoch']=epoch
+            val['ctr']=global_ctr
+            validation_log.append(val)
 
-            print('Epoch: {} Validation Loss: {:.4f} Acc: {:.4f} TPR: {:.4f} TNR: {:.4f} FPR:{:.4f}'.\
-                    format(epoch, val_loss, val_acc, val_tp, val_tn,val_fp))
-                
-        if not params.adam:
-            scheduler.step()
-        
-        if val_acc > best_acc:
-                best_acc = val_acc
-                best_model_acc = copy.deepcopy(model.state_dict())
-                save_model(model,"best_acc",params.output,fold=params.fold,folds=params.folds)
+            print('Epoch: {} Validation Loss: {:.4f} ACC:{:.4f} TPR:{:.4f} TNR:{:.4f} AUC:{:.4f}'.\
+                    format(epoch, val['loss'], val['acc'], val['tpr'], val['tnr'],val['auc']))
+        else:
+            print('Epoch: {} no validation'.format(epoch))
 
-        if val_tp > best_tpr:
-                best_tpr = val_tp
-                best_model_tpr = copy.deepcopy(model.state_dict())
-                save_model(model,"best_tpr",params.output,fold=params.fold,folds=params.folds)
 
-        if val_tn > best_tnr:
-                best_tnr = val_tn
-                best_model_tnr = copy.deepcopy(model.state_dict())
-                save_model(model,"best_tnr",params.output,fold=params.fold,folds=params.folds)
-
+    final_model = copy.deepcopy(model.state_dict())
     save_model(model,"final", params.output,fold=params.fold,folds=params.folds)
-    
-    # get the best models
-    if False:
+
+    if len(validation)>0:
         model.load_state_dict(best_model_acc)
-        model=model.cpu()
-        save_model(model,"best_acc_cpu",params.output)
+        save_model(model,"best_acc",params.output)
         
         model.load_state_dict(best_model_tpr)
-        model=model.cpu()
-        save_model(model,"best_tpr_cpu",params.output)
-        
+        save_model(model,"best_tpr",params.output)
+            
         model.load_state_dict(best_model_tnr)
-        model=model.cpu()
-        save_model(model,"best_tnr_cpu",params.output)
-    
+        save_model(model,"best_tnr",params.output)
+        
+        model.load_state_dict(best_model_auc)
+        save_model(model,"best_auc",params.output)
+
+    testing_final={}
+    testing_best_acc={}
+    testing_best_tpr={}
+    testing_best_tnr={}
+    testing_best_auc={}
+
     if len(testing)>0:
+        print("Testing...")
         testing_dataloader = DataLoader(testing_dataset, 
                             batch_size=1,
                             shuffle=False, 
@@ -288,90 +310,43 @@ if __name__ == '__main__':
                             drop_last=False)
 
         with torch.no_grad():
-            val_loss  = 0.0
-            val_acc   = 0.0
 
-            val_tp    = 0.0
-            val_tn    = 0.0
-            val_ap    = 0.0
-            val_an    = 0.0
-            val_fp    = 0.0
+            model.load_state_dict(final_model)
+            testing_final = run_validation_testing_loop(testing_dataloader, model, details=True)
 
-            for v_batch, v_sample_batched in enumerate(testing_dataloader):
-                inputs = v_sample_batched['image' ].cuda()
-                labels = v_sample_batched['status'].cuda()
+            if len(validation)>0:
+                model.load_state_dict(best_model_acc)
+                testing_best_acc = run_validation_testing_loop(testing_dataloader, model, details=True)
 
-                outputs = model(inputs)
-                loss = nn.functional.cross_entropy(outputs, labels)
-                outputs = nn.functional.softmax(outputs,1)
-                _, preds = torch.max(outputs, 1)
+                model.load_state_dict(best_model_auc)
+                testing_best_auc = run_validation_testing_loop(testing_dataloader, model, details=True)
 
-                testing_details.append(
-                    {
-                        'id':    v_sample_batched['id'][0],
-                        'label': int(labels[0]),
-                        'raw' :  float(outputs[0,1]),
-                        'pred':  int(preds[0])
-                    }
-                )
+                model.load_state_dict(best_model_tpr)
+                testing_best_tpr = run_validation_testing_loop(testing_dataloader, model, details=True)
 
-                val_loss += float(loss) * inputs.size(0)
-                val_acc  += float(torch.sum(preds == labels))
+                model.load_state_dict(best_model_tnr)
+                testing_best_tnr = run_validation_testing_loop(testing_dataloader, model, details=True)
 
-                # calculating true positive and true negative
-                val_tp   += float(torch.sum( (preds == 1)*(labels==1)))
-                val_fp   += float(torch.sum( (preds == 1)*(labels==0)))
-                val_tn   += float(torch.sum( (preds == 0)*(labels==0)))
-
-                val_ap   += float(torch.sum( (labels == 1)))
-                val_an   += float(torch.sum( (labels == 0)))
-
-            # (?)
-            val_loss /= len(testing_dataset)
-            val_acc  /= len(testing_dataset)
-
-            if val_ap>0:
-                val_tp  /= val_ap
-            else:
-                val_tp = 0.0
-
-            if val_an>0:
-                val_tn  /= val_an
-                val_fp  /= val_an
-            else:
-                val_tn = 0.0
-            
-            print("{} - {},{}".format(global_ctr, val_loss, val_acc))
-
-            log = {'loss':  val_loss,
-                    'acc':  val_acc,
-                    'tpr':  val_tp,
-                    'tnr':  val_tn,
-                    'fpr':  val_fp
-                    }
-
-            testing_log.append(log)
-
-            print('Testing Loss: {:.4f} Acc: {:.4f} TPR: {:.4f} TNR: {:.4f} FPR:{:.4f}'.\
-                    format(val_loss, val_acc, val_tp, val_tn, val_fp))
 
     log_path = os.path.join(params.output, 'log_{}_{}.json'.format(params.fold,params.folds))
+
     print("Saving log to {}".format(log_path))
     with open(log_path,'w') as f:
         json.dump(
             {
                 'folds': params.folds,
                 'fold': params.fold,
+
                 'model': params.net,
                 'ref': params.ref,
                 'batch_size': params.batch_size,
                 'n_epochs': params.n_epochs,
+
                 'training':training_log,
                 'validation': validation_log,
-                'testing': testing_log,
-                'testing_details': testing_details
+
+                'testing_best_acc': testing_best_acc,
+                'testing_best_auc': testing_best_auc,
+                'testing_best_tpr': testing_best_tpr,
+                'testing_best_tnr': testing_best_tnr
             }, f  )
-        
-
-
-    
--- python/run_cv_experiments.sh
+++ python/run_cv_experiments.sh
@@ -2,8 +2,8 @@
 
 FOLDS=8
 
-for m in sq101,20,128 \
-         r18,20,128 ;do
+# sq101,20,128
+for m in r18,20,196 ;do
  i=( ${m//,/ } )
  for f in $(seq 0 $((${FOLDS}-1)) );do
    python aqc_training.py --fold $f --folds $FOLDS --net ${i[0]} --adam --n_epochs ${i[1]} --batch_size ${i[2]}  model_${i[0]} 2>&1 |tee log_${i[0]}_${f}_${FOLDS}.txt
