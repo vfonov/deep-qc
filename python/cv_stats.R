76678e3f22b74f4b17e8819dcee7d6b9c996389a
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2021-05-10 15:28:41 -0400

    WIP: analyzing CV results

--- python/cv_stats.R
+++ python/cv_stats.R
@@ -3,35 +3,47 @@ library(jsonlite)
 
 cv<-data.frame()
 
-for(v in c('r18','sq101')){
+# all these are pretrained
+for(v in c('r18')){
     for(r in c('_ref','')) {
         for(f in seq(0,7)) {
             rrr=fromJSON(paste0('model_',v,r,'/log_',f,'_8.json'))
 
             fold <- bind_rows(
-                as.data.frame(rrr$testing_best_acc$summary)%>%mutate(select_kind='best_acc'),
-                as.data.frame(rrr$testing_best_auc$summary)%>%mutate(select_kind='best_auc'),
-                as.data.frame(rrr$testing_best_tpr$summary)%>%mutate(select_kind='best_tpr'),
-                as.data.frame(rrr$testing_best_tnr$summary)%>%mutate(select_kind='best_tnr')) %>%
+                as.data.frame(rrr$testing_final$summary)   %>%mutate(select_kind='final'),
+                as.data.frame(rrr$testing_best_acc$summary)%>%mutate(select_kind='acc'),
+                as.data.frame(rrr$testing_best_auc$summary)%>%mutate(select_kind='auc'),
+                as.data.frame(rrr$testing_best_tpr$summary)%>%mutate(select_kind='tpr'),
+                as.data.frame(rrr$testing_best_tnr$summary)%>%mutate(select_kind='tnr')) %>%
                 mutate(fold=f,ref=(r=="_ref"),model=v)
             cv<-bind_rows(cv,fold)
         }
     }
 }
 
-ccv<-cv %>% gather(`acc`,`tpr`,`tnr`,`auc`,key='measure',value='score')
-tccv<-ccv %>% group_by(measure,model,ref,select_kind) %>% 
- summarize(score=median(score),score_lab=signif(score,4))
+# all these are pretrained
+cv<-cv %>% mutate(pre=T, select_kind=factor(select_kind,levels=c('final', 'auc', 'acc', 'tpr', 'tnr') ))
+
+ccv<-cv %>% gather(`acc`,`tpr`,`tnr`,`auc`, key='measure', value='score')
+tccv<-ccv %>% group_by(measure, model, ref, select_kind, pre) %>% 
+  summarize( score=median(score), score_lab=signif(score,4)) %>%ungroup()
+
+# select the best TNR
+best_tnr<-max( (tccv%>%filter(measure=='tnr'))$score)
+
+# highlite
+tccv<-tccv %>% mutate( highlite=(score==best_tnr)&(measure=='tnr') )
 
 #     geom_hline(data=tccv,aes(x=measure,yintercept=score))+
 
 
-png("DARQ_CV_performance_20epochs.png", width=2000, height=2000)
+png("DARQ_CV_performance_10epochs_resnet18.png", width=2000, height=1000)
 
+#    theme(axis.text.x=element_text(angle=45,vjust=0.3))+
 ggplot(ccv,aes(y=score,x=select_kind))+
-    theme_bw(base_size = 25)+
-    theme(axis.text.x=element_text(angle=45,vjust=0.3))+
+    theme_bw(base_size = 28)+
     geom_boxplot()+
-    facet_wrap(measure~ref+model, labeller='label_both')+
-    geom_text(data=tccv,aes(label=score_lab,x=select_kind,y=score),size=5,nudge_y=0.03)+
-    xtitle('')
+    facet_wrap(ref~measure, labeller='label_both',ncol=4)+
+    geom_text(data=tccv, aes(label=score_lab, x=select_kind, y=score,color=highlite),show.legend = F,size=6,nudge_y=0.03)+
+    scale_colour_manual(labels=c(F,T),values=c('black','red'))+
+    ggtitle('Model: Resnet-18 pretrained on ImageNet with and without reference')
--- python/run_cv_experiments.sh
+++ python/run_cv_experiments.sh
@@ -6,7 +6,7 @@ FOLDS=8
 for m in r18,10,196 ;do
  i=( ${m//,/ } )
  for f in $(seq 0 $((${FOLDS}-1)) );do
-   python aqc_training.py --fold $f --pretrained --folds $FOLDS --net ${i[0]} --adam --n_epochs ${i[1]} --batch_size ${i[2]}  model_${i[0]} 2>&1 |tee log_${i[0]}_${f}_${FOLDS}.txt
-   python aqc_training.py --ref --fold $f --pretrained --folds $FOLDS --net ${i[0]} --adam --n_epochs ${i[1]} --batch_size ${i[2]}  model_${i[0]}_ref --ref 2>&1 |tee log_ref_${i[0]}${f}_${FOLDS}.txt
+   python aqc_training.py --lr 0.00001 --fold $f --pretrained --folds $FOLDS --net ${i[0]} --adam --n_epochs ${i[1]} --batch_size ${i[2]}  model_${i[0]} 2>&1 |tee log_${i[0]}_${f}_${FOLDS}.txt
+   python aqc_training.py --lr 0.00001 --ref --fold $f --pretrained --folds $FOLDS --net ${i[0]} --adam --n_epochs ${i[1]} --batch_size ${i[2]}  model_${i[0]}_ref --ref 2>&1 |tee log_ref_${i[0]}${f}_${FOLDS}.txt
  done
 done
