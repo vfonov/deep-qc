9bf59e83be04e58f1290ccb782f774fa1aad9264
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2021-05-11 00:45:22 -0400

    Added -pretrained flag

--- python/run_all_experiments.sh
+++ python/run_all_experiments.sh
@@ -9,7 +9,7 @@ for m in sq101,20,64 \
          r152,40,8 ;do
  i=( ${m//,/ } )
  if [ ! -e model_${i[0]}/final.pth ];then
- python aqc_training.py --net ${i[0]} --adam --n_epochs ${i[1]} --batch_size ${i[2]}  model_${i[0]} 2>&1 |tee log_${i[0]}.txt
+ python aqc_training.py --pretrained --save_final --save_best --net ${i[0]} --adam --n_epochs ${i[1]} --batch_size ${i[2]}  model_${i[0]} 2>&1 |tee log_${i[0]}.txt
  fi
 
 done
@@ -23,7 +23,7 @@ for m in sq101,20,64 \
          r152,40,8 ;do
  i=( ${m//,/ } )
  if [ ! -e model_${i[0]}_ref/final.pth ];then
- python aqc_training.py --net ${i[0]} --adam --n_epochs ${i[1]} --batch_size ${i[2]}  model_${i[0]}_ref --ref 2>&1 |tee log_${i[0]}.txt
+ python aqc_training.py --pretrained --save_final --save_best --net ${i[0]} --adam --n_epochs ${i[1]} --batch_size ${i[2]}  model_${i[0]}_ref --ref 2>&1 |tee log_${i[0]}.txt
 
  fi
 
